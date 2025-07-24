363ad71dcb0956407e21ed56532dc808ce1cd0ba
aziz3r <mohamed.baoueb@insa-cvl.fr>
2025-07-24 11:57:53 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // src/App.tsx
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
+import './assets/styles/global.css'; // Ton style global
+import './locales/i18n'; // Initialisation i18next
 
 function App() {
   return <AppRoutes />;
 }
 
 export default App;
-
-

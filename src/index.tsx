ed2a932d31a11036dea96b9181223f6930f45b6e
aziz3r <mohamed.baoueb@insa-cvl.fr>
2025-07-24 12:13:03 +0100

    normalement correct

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-// src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import './locales/i18n';
+import './assets/styles/global.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
